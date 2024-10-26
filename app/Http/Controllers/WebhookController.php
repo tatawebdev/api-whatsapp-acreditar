023d8e8d421e1680ee86347c32321f77dce2988d
tatawebdev <suporte@tataweb.com.br>
2024-10-26 10:18:03 -0300

    webHook

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -10,20 +10,52 @@ class WebhookController extends Controller
 {
     public function processWebhook(Request $request)
     {
+        if ($this->isChallengeRequest($request)) {
+            return $this->handleChallenge($request);
+        }
+
         $data = $request->getContent();
         $result = WebhookProcessor::tratarWebhookWhatsApp($data);
+
+        $this->saveWebhookData($request->all());
+
         return response()->json($result);
     }
 
     public function processWebhookMockado(Request $request)
     {
+        if ($this->isChallengeRequest($request)) {
+            return $this->handleChallenge($request);
+        }
 
         WebhookProcessor::debugOn();
 
         $data = $request->getContent();
         $result = WebhookProcessor::tratarWebhookWhatsApp($data);
 
+        // Para depuração, exibindo o resultado
         dd($result);
         // return response()->json($result);
+    }
+
+    // Verifica se é uma solicitação de desafio do webhook
+    private function isChallengeRequest(Request $request): bool
+    {
+        return $request->has('hub_challenge');
+    }
+
+    // Lida com a solicitação de desafio
+    private function handleChallenge(Request $request)
+    {
+        return response($request->input('hub_challenge'), 200);
+    }
+
+    // Salva os dados do webhook em um arquivo JSON
+    private function saveWebhookData(array $data): void
+    {
+        $webhookData = json_encode($data);
+        $filePath = 'webhooks/webhook_' . now()->timestamp . '.json';
+
+        Storage::disk('webhooks')->put($filePath, $webhookData);
     }
 }
